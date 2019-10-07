31c54dd7985da71a3c70436a7ecff92467e16a6e
mflJefferson <mflJefferson@gmail.com>
2019-10-07 20:42:09 -0300

    feat: login

--- src/br/com/modelo/fatec/Cliente.java
+++ src/br/com/modelo/fatec/Cliente.java
@@ -1,5 +1,71 @@
 package br.com.modelo.fatec;
 
 public class Cliente {
+	protected String nome;
+	protected String telefone;
+	protected String endereco;
+	protected String email;
+	protected String user;
+	protected String password;
+	
+	public Cliente(String nome, String telefone, String endereco, String email, String user, String password) {
+		this.nome = nome;
+		this.telefone = telefone;
+		this.endereco = endereco;
+		this.email = email;
+		this.user = user;
+		this.password = password;
+	}
 
+	public Cliente() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public String getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getUser() {
+		return user;
+	}
+
+	public void setUser(String user) {
+		this.user = user;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
 }
